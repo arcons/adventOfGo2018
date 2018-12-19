e16763768bc2f490cf87dafe805f0b3149dc0b0a
AlexConsunji <alexander.consunji@slalom.com>
2018-12-18 20:43:35 -0500

    Make a create a ring in order

--- day9/day9.go
+++ day9/day9.go
@@ -1,47 +1,27 @@
 package main
 
-import(
-	"fmt"
-	"container/ring"
-)
-
-func main(){
-	package main
-
 import (
 	"container/ring"
 	"fmt"
-	"os"
-	"time"
 )
 
 //internet ring example
 func main() {
-	coffee := []string{"kenya", "guatemala", "ethiopia"}
+	// numPlayers := 439
+	// lastMarble := 71307
+	numPlayers := 9
+	lastMarble := 25
+	fmt.Println(numPlayers, lastMarble)
 
 	// create a ring and populate it with some values
-	r := ring.New(len(coffee))
-	for i := 0; i < r.Len(); i++ {
-		r.Value = coffee[i]
+	r := ring.New(lastMarble)
+	r.Value = 0
+	//populate the ring
+	fmt.Println(r)
+	for i := 1; i <= lastMarble; i++ {
 		r = r.Next()
+		r.Value = i
+		fmt.Println(r)
+		//use the move method to move back 7 spaces then pop off the marble 7 back
 	}
-
-	// print all values of the ring, easy done with ring.Do()
-	r.Do(func(x interface{}) {
-		fmt.Println(x)
-	})
-
-	// end after 10s otherwise play.golang.org gives an error
-	go func() {
-		time.Sleep(time.Second * 10)
-		fmt.Println("-- exiting --")
-		os.Exit(0)
-	}()
-
-	// .. or each one by one.
-	for _ = range time.Tick(time.Second * 1) {
-		r = r.Next()
-		fmt.Println(r.Value)
-	}
-}
 }
